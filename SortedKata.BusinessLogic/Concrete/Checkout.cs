58a08cdadbfc38166cc538b68f851121fd52b1ae
Bob Hall <myk_quayce@hotmail.com>
2019-02-18 23:23:46 +0000

    Adding Checkout.Scan logic.

--- SortedKata.BusinessLogic/Concrete/Checkout.cs
+++ SortedKata.BusinessLogic/Concrete/Checkout.cs
@@ -1,15 +1,27 @@
-﻿using System;
+﻿using Dawn;
+using System;
 using System.Collections.Generic;
 
 namespace SortedKata.BusinessLogic.Concrete
 {
 	public class Checkout : ICheckout
 	{
+		private const string _itemPattern = "^[A-Za-z][0-9]{2}$";
+
 		public IDictionary<string, int> Basket { get; } = new Dictionary<string, int>();
 
 		public void Scan(string sku)
 		{
-			throw new NotImplementedException();
+			Guard.Argument(() => sku).NotNull().NotEmpty().NotWhiteSpace().Matches(_itemPattern);
+
+			if (Basket.ContainsKey(sku))
+			{
+				Basket[sku]++;
+			}
+			else
+			{
+				Basket.Add(sku, 1);
+			}
 		}
 	}
 }
